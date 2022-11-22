b4c0bbba7fdf3ced4b4d994c1141b935be670e5d
bixTry <cccrankboy@gmail.com>
2022-11-22 17:59:48 +0600

    app

---
+++ phpBack/API.php
@@ -0,0 +1,8 @@
+<?php
+include_once "models/Employee.php";
+include_once "API.php";
+
+header("server access-control-allow-origin:*");
+
+
+
--- phpBack/GetDate.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "models/Employee.php";
-include_once "GetDate.php";
-
-
-
-
