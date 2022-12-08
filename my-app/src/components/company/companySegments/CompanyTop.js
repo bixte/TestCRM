1306a29354d748bf244b036a7c3fa2f552cb39e4
bixTry <cccrankboy@gmail.com>
2022-12-08 12:13:57 +0600

    add table column menu

--- my-app/src/components/company/companySegments/CompanyTop.js
+++ my-app/src/components/company/companySegments/CompanyTop.js
@@ -1,8 +1,11 @@
 import React from "react";
-import {Button, Table} from "reactstrap";
+import { Table} from "reactstrap";
 import {TableHeader} from "../materials/table/TableHeader";
 import {TableBody} from "../materials/table/TableBody";
 import {CompanyTopCreateForm} from "./Form/top/CompanyTopCreateForm";
+import TableNavigation from "../materials/table/TableNavigation";
+import TableMenu from "../materials/table/TableMenu";
+import TableManage from "../materials/table/TableManage";
 
 let heads = ["Id", "Фамилия", "Имя", "Отчество", "Дата рождения", "Город"]
 
@@ -10,17 +13,12 @@ export class CompanyTop extends React.Component {
     render() {
         return (
             <div className={"app-company-top position-relative"}>
-                <div className={"app-company-top-content "}>
+                <div className={"app-company-top-content"}>
                     <Table className={"app-company-top__table table-bordered table-hover table-responsive"}>
                         <TableHeader heads={heads}/>
                         <TableBody changeCurrentRow={this.props.changeCurrentRow} data={this.props.data}/>
                     </Table>
-                    <div className={"position-absolute app-company-top-create"}>
-                        <Button className={"btn btn-success p-2"} data-bs-toggle={"collapse"}
-                                data-bs-target={".app-company-top-content-create"} aria-expanded="false"
-                                aria-controls="app-company-top-content-create">Create
-                        </Button>
-                    </div>
+                    <TableManage/>
                     <div className={'app-company-top-content-create collapse'}>
                         <CompanyTopCreateForm onchangeData={this.props.onchangeData}/>
                     </div>
